a7132daa755f65ad50756531b64d46234101ee47
hossemedineali <hossemedine@outlook.com>
2023-03-27 03:34:44 +0100

    return to flip  slider

--- src/pages/components/newService.tsx
+++ src/pages/components/newService.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y,Autoplay ,} from 'swiper';
-import { EffectCards, EffectFlip,EffectCube} from "swiper";
+import { EffectCards, EffectFlip,} from "swiper";
 import {motion} from 'framer-motion'
-
-
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import "swiper/css/effect-cards";
+import "swiper/css/effect-flip";
+import "swiper/css";
+import "swiper/css/effect-flip";
 import "swiper/css/pagination";
-import "swiper/css/effect-cube";
-
+import "swiper/css/navigation";
 
 /*
 className='h-96 w-full max-w-xs mx-auto'
@@ -22,29 +25,22 @@ function App() {
     const services = ['Policies and Procedures', 'Customized Employee Handbook', 'Benefits Review', 'Positive Employee Relations', 'Arbitrations', 'NLRB Actions'];
     const colors=['red','blue','green','yellow','brown','grey']
   return (
-    <div className=' h-full flex flex-col md:px-20 px-8 pt-10'>
+    <div className=' h-full flex flex-col md:px-20 px-8 gap-10 pt-10'>
         <h1 className='text-3xl text-center py-2 font-semibold font-serif'>Our Services</h1>
          <Swiper
-        effect={"cube"}
+        effect={"flip"}
         grabCursor={true}
         pagination={true}
-        navigation={false}
-        loop={true}
-       cubeEffect={{
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      }}
-      
+        navigation={true}
+       // loop={true}
         
-        modules={[ Pagination, Navigation,EffectCube]}
-        className="mySwiper relative h-96 w-full max-w-xs mx-auto"
+        modules={[EffectFlip, Pagination, Navigation,]}
+        className="mySwiper relative h-96 w-full max-w-md mx-auto"
    
     >
         {services.map((item,index)=>{
             return       <SwiperSlide key={index}  className='h-full block  w-10/12
-            bg-slate-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 border border-gray-100
+            bg-stone-700 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 border border-gray-100
 
              shadow-xl   hover:cursor-pointer '>
                 <motion.span initial={{x:10,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:2,delay:1}}>
